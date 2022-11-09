3ec1be20d470c6a3086aefeef421707ef3cf2c1f
Riccardo Ferranti <riccardoferranti@MacBook-Pro-di-Riccardo.local>
2022-11-10 00:29:32 +0100

    Updated title

--- src/Layout/Header.tsx
+++ src/Layout/Header.tsx
@@ -1,15 +1,16 @@
 import { FC } from 'react';
 import { Link } from 'react-router-dom';
-import Search from '../components/Filters/Search';
 
 import { StyledHeader, StyledLogo, StyledHeaderContainer, StyledNav } from './Header.style';
+
+import Search from '../components/Filters/Search';
 import Cart from '../components/Cart/Cart';
 
 const Header: FC = () => (
   <StyledHeader>
     <StyledHeaderContainer>
       <StyledLogo>
-        <Link to="/">Simply Store</Link>
+        <Link to="/">Online Store</Link>
       </StyledLogo>
       <Search />
       <StyledNav>
